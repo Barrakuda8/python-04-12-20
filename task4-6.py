61e730866a371717a311a416869190c7113a3072
Barrakuda <brpslelush@gmail.com>
2021-01-13 18:28:59 +0100

    second commit

--- task4-6.py
+++ task4-6.py
@@ -1,12 +1,14 @@
 from random import randint, choice
-import re
+from string import ascii_uppercase
+import typing
+
 
 class Warehouse:
 
     def __init__(self, data: dict):
         self.data = data
 
-    def add_item(self, equipment, item):
+    def add_item(self, equipment: str, item: OfficeEquipment()):
         self.data[equipment.lower()].append(item)
 
     def transport_by_sn(self, other, equipment, serial_num):
@@ -20,18 +22,38 @@ class Warehouse:
                         other.data[equipment] = []
                     other.data[equipment].append(self.data[equipment][i].pop(i))
 
-    def transport_by_quantity(self, other, equipment, quantity: int):
+    def transport_by_quantity(self, other, equipment, quantity: int, **kwargs):
         equipment = equipment.lower()
         if equipment not in self.data:
             print("На этом складе нет этого товара")
+        elif len(self.data[equipment]) < quantity:
+            print(f"Недостаточно единиц товара: {len(self.data[equipment])}")
         else:
-            if len(self.data[equipment]) <= quantity:
-                print(f"Недостаточно товара. Всего на этом складе осталось единиц товара: {len(self.data[equipment])}")
+            to_transport = []
+            for i in range(self.data[equipment]):
+                if len(to_transport) == quantity:
+                    break
+                for key, value in kwargs:
+                    n = 0
+                    if self.data[equipment][i].key == value:
+                        n += 1
+                    elif n == len(kwargs):
+                        to_transport.append(self.data[equipment][i].pop())
+                    else:
+                        break
+            if len(to_transport) <= quantity:
+                answer = input(f"Недостаточно единиц подходящего товара: {len(to_transport)}\n"
+                               f"Перевезти имеющееся количество подходящего товара? ('Да' или 'Нет')\n>>> ")
+                while True:
+                    if answer.lower() == "да":
+                        to_transport.extend(other.data[equipment])
+                        break
+                    elif answer.lower() == "нет":
+                        to_transport.extend(self.data[equipment])
+                    else:
+                        answer = input("Введите 'Да' или 'Нет'\n>>> ")
             else:
-                if equipment not in other.data:
-                    other.data[equipment] = []
-                for i in range(quantity):
-                    other.data[equipment].append(self.data[equipment].pop(i))
+                to_transport.extend(other.data[equipment])
 
 
 class OfficeEquipment:
@@ -43,20 +65,46 @@ class OfficeEquipment:
         self.warranty = warranty
         self.color = color
 
+    @staticmethod
+    def generate_serial_num():
+        return f"{randint(1000, 9999)}-{''.join(choice(ascii_uppercase) for _ in range(4))}-{randint(1000, 9999)}"
+
+    @staticmethod
+    def generate_model():
+        return f"{choice(ascii_uppercase)}"
+
+    @staticmethod
+    def generate_year():
+        return randint(2015, 2020)
+
+    @staticmethod
+    def generate_warranty():
+        return randint(1, 5)
+
+    @staticmethod
+    def generate_color():
+        return choice(["black", "white", "grey"])
+
     @classmethod
     def generate(cls):
-        serial_num = f"{randint(1000, 9999)}-{choice(r'[A-Z]{4}')}-{randint(1000, 9999)}"
-        model = f"{choice(r'[A-Z]')}"
-        year = randint(2015, 2020)
-        warranty = randint(1, 5)
-        color = choice(["black", "white", "grey"])
-        return cls(serial_num, model, year, warranty, color)
+        return cls(cls.generate_serial_num(), cls.generate_model(), cls.generate_year(),
+                   cls.generate_warranty(), cls.generate_color())
 
 
 class Printer(OfficeEquipment):
 
-    def __init__(self, serial_num, model, year, warranty, color):
+    def __init__(self, serial_num, model, year, warranty, color, bw_or_color):
         super().__init__(serial_num, model, year, warranty, color)
+        self.bw_or_color = bw_or_color
+
+    @staticmethod
+    def generate_bw_or_color():
+        return choice(["bw", "color"])
+
+    @classmethod
+    def generate(cls):
+        return cls(cls.generate_serial_num(), cls.generate_model(), cls.generate_year(),
+                   cls.generate_warranty(), cls.generate_color(), cls.generate_bw_or_color())
 
 
 class Scanner(OfficeEquipment):
@@ -73,7 +121,9 @@ class CopyMachine(OfficeEquipment):
 
 warehouse_a = Warehouse({})
 warehouse_b = Warehouse({"printer": [Printer.generate(), Printer.generate(), Printer.generate()]})
-print(warehouse_b.data["printer"][0].serial_num)
+print(warehouse_b.data["printer"][0].serial_num, warehouse_b.data["printer"][0].model,
+      warehouse_b.data["printer"][0].color, warehouse_b.data["printer"][0].warranty,
+      warehouse_b.data["printer"][0].year)
 warehouse_b.transport_by_quantity(warehouse_a, "printer", 2)
 print(warehouse_b.data, warehouse_a.data)
 warehouse_b.transport_by_quantity(warehouse_a, "printer", 2)
